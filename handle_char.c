5af0b6ed2cd04fb91633751b3e20547e50a983c7
Rabbouz <rabbouzzakr@gmai.com>
2023-07-25 17:06:47 -0700

    yarbi ydoz

--- handle_char.c
+++ handle_char.c
@@ -2,23 +2,13 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
  * handle_char - A function that prints a character
  * @c: argument
  * Return: number of characters printed (1)
  */
 
-int handle_char(char *c)
-{
-	_putchar(*c);
-=======
- * handle_char - A  function that prints a character
- * @s: argument
- * Return: number of characters printed (1)
- */
 int handle_char(char *s)
 {
 	_putchar(*s);
->>>>>>> de2823ed81f18925bb5fff7ba96270a73b823488
 	return (1);
 }
